bcd4687e9b9efa08787b1ed4d3489d41964f5196
AbdAlfadeel-Hamdy <abdalfadeelh@gmail.com>
2022-09-06 17:19:47 +0200

    Refactoring Form Validation

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,10 +1,38 @@
-const SimpleInput = (props) => {
+import { useState } from "react";
+
+const SimpleInput = () => {
+  const [enteredName, setEnteredName] = useState("");
+  const [inputNameIsTouched, setinputNameIsTouched] = useState(false);
+  const enterdNameIsValid = enteredName.trim() !== "";
+  const inputNameChangeHandler = (e) => setEnteredName(e.target.value);
+  const inputNameBluronBlurHandler = () => setinputNameIsTouched(true);
+  const formSubmissionHandler = (e) => {
+    e.preventDefault();
+    setinputNameIsTouched(true);
+    if (!enterdNameIsValid) return;
+    console.log(enteredName);
+    setEnteredName("");
+    setinputNameIsTouched(false);
+  };
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmissionHandler}>
+      <div
+        className={`form-control ${
+          !enterdNameIsValid && inputNameIsTouched ? "invalid" : ""
+        }`}
+      >
+        <label htmlFor="name">Your Name</label>
+        <input
+          type="text"
+          id="name"
+          onChange={inputNameChangeHandler}
+          onBlur={inputNameBluronBlurHandler}
+          value={enteredName}
+        />
       </div>
+      {!enterdNameIsValid && inputNameIsTouched && (
+        <p className="error-text">Name must not be empty!</p>
+      )}
       <div className="form-actions">
         <button>Submit</button>
       </div>
